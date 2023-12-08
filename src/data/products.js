39de1ccc44ae443163aa3e82b7f45ffe85b3afa7
maryam <shabanzadeh@yahoo.com>
2023-12-08 12:19:52 +0100

    create products

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+import mobile from '../assets/iphone-14.jpeg'
+import laptop from '../assets/Apple-MacBook-Pro-M2.jpg.jpg'
+export const  PRODUCTS = [
+    {
+        id: 1,
+        ProductName: 'Mobile',
+        Peice: 1000,
+        ProductImage: mobile
+    },
+    {
+       id: 2,
+       ProductName: 'Laptop',
+       peice: 3000,
+       ProductImage: laptop
+    }
+]

9ae75516178e1880b3fba0a2896364a5c9a12dd2
maryam <shabanzadeh@yahoo.com>
2023-12-08 09:14:07 +0100

    create nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,18 @@
+import {Link} from 'react-router-dom'
 const Nav =()=>{
     return(
-        <h1>Nav</h1>
+       <div className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div className="container">
+            <a className="navbar-brand"> Maryam</a>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className='nav-link'>Shop</Link>
+                </li>
+                <li className="nav-item"><Link to="/cart" className='nav-link'>Cart</Link></li>
+            </ul>
+
+        </div>
+       </div>
     )
 
 }

d6ebac3e12eaf3c9e1f9cbaa5a5905049b935389
maryam <shabanzadeh@yahoo.com>
2024-01-23 10:53:57 +0100

    added neu product to cart

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -7,7 +7,14 @@ export const ShopContextProvider = (props)=>{
     const [cartItems , setCartItems] = useState([])
 
     const addToCart = (itemId)=>{
+        if(!cartItems?.find((item)=>item.id===itemId))
         setCartItems([...cartItems, {id : itemId, count:1}])
+        else
+        setCartItems(cartItems.map((item)=>{
+            if(item.id===itemId)
+                return{...item, count : item.count +1}
+                else return item
+    }))
     }
     const removeFromCart = (itemId)=>{
         setCartItems(cartItems.map((i)=>{

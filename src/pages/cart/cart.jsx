a7e83dbd1106ecd30708b7a45803dc0fb86b06e6
maryam <shabanzadeh@yahoo.com>
2024-01-29 14:38:20 +0100

    create a buyer cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+
+import {PRODUCTS} from "../../data/products"
+import { ShopContext } from "../../context/shopContext";
+import Product from "../shop/product";
+import React from "react";
+
 const Card =()=>{
+    const{cartItems} = useContext(ShopContext)
     return(
         <React.Fragment>
         <h1>Your Cart items</h1>
         <div className="row">
+            {PRODUCTS.map((p)=>{
+                if(cartItems.some((i)=> i.id===p.id && i.count>0))
+                return <Product data={p}></Product>
+            })}
+          
+            
 
         </div>
         </React.Fragment>
